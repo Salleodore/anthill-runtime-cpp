4ae5de59a49fd09b090217d2c1fd885c0027e119
Tinaynox <n.d.a@mail.ru>
2018-03-13 13:12:12 +0200

    ValidateExternalAuthenticationState stored access token fixed

--- src/anthill/states/ValidateExternalAuthenticationState.cpp
+++ src/anthill/states/ValidateExternalAuthenticationState.cpp
@@ -30,90 +30,91 @@ namespace online
 		AnthillRuntime& online = AnthillRuntime::Instance();
 		StoragePtr storage = online.getStorage();
 		
-		const ListenerPtr& listener = AnthillRuntime::Instance().getListener();
+		
 
-		if (listener && !listener->shouldSaveExternalStorageAccessToken())
+		
+
+		if (storage->has(Storage::StorageAccessTokeneField))
 		{
-			Log::get() << "Local access token is not used by external authenticator." << std::endl;
-			switchTo<ExternalAuthenticationState>();
+			Log::get() << "Validating (probably external) access token." << std::endl;
+
+			std::string accessToken = storage->get(Storage::StorageAccessTokeneField);
+			
+			lock();
+
+			ptr->validateAccessToken(accessToken, 
+				[this, accessToken](const LoginService& service, Request::Result result, const Request& request,
+					const std::string& credential, const std::string& account, const LoginService::Scopes& scopes)
+			{
+				unlock();
+        
+				LoginServicePtr ptr = AnthillRuntime::Instance().get<LoginService>();
+				ptr->setCurrentAccessToken(accessToken);
+
+				const online::ExternalAuthenticatorPtr& authenticator = ptr->getExternalAuthenticator();
+
+				if (request.isSuccessful())
+				{
+					online::Credential parsed(credential);
+
+					if (parsed.getCredentialType() != authenticator->getCredentialType())
+					{
+						Log::get() << "Credential types don't match! ";
+						Log::get() << "Parsed credential type=" << parsed.getCredentialType();
+						Log::get() << "External credential type=" << authenticator->getCredentialType() << std::endl;
+						switchTo<AttachExternalAuthenticationState>();
+						return;
+					}
+
+					const ListenerPtr& listener = AnthillRuntime::Instance().getListener();
+					if (listener && !listener->shouldSaveExternalStorageAccessToken())
+					{
+						Log::get() << "Local access token is not used by external authenticator." << std::endl;
+						switchTo<ExternalAuthenticationState>();
+						return;
+					}
+
+					const ApplicationInfo& applicationInfo = AnthillRuntime::Instance().getApplicationInfo();
+                
+					// check if the requiredScopes are satisfied
+					if (std::includes(scopes.begin(), scopes.end(), applicationInfo.shouldHaveScopes.begin(), applicationInfo.shouldHaveScopes.end()))
+					{
+						Log::get() << "Access token is valid!" << std::endl;
+
+						const ListenerPtr& listener = AnthillRuntime::Instance().getListener();
+
+						if (listener)
+						{
+							listener->authenticated(account, credential, scopes);
+						}
+
+						complete();
+						return;
+					}
+					else
+					{
+						Log::get() << "Required scopes are not satisfied!" << std::endl;
+					}
+				}
+   
+				Log::get() << "Access token is invalid!" << std::endl;
+            
+				AnthillRuntime& online = AnthillRuntime::Instance();
+				StoragePtr storage = online.getStorage();
+
+				storage->remove(Storage::StorageAccessTokeneField);
+				storage->save();
+
+				switchTo<ExternalAuthenticationState>();
+			});
+
+
 		}
 		else
 		{
+			Log::get() << "Access token is not found." << std::endl;
 
-			if (storage->has(Storage::StorageAccessTokeneField))
-			{
-				Log::get() << "Validating (probably external) access token." << std::endl;
-
-				std::string accessToken = storage->get(Storage::StorageAccessTokeneField);
-			
-				lock();
-
-				ptr->validateAccessToken(accessToken, 
-					[this, accessToken](const LoginService& service, Request::Result result, const Request& request,
-						const std::string& credential, const std::string& account, const LoginService::Scopes& scopes)
-				{
-					unlock();
-        
-					LoginServicePtr ptr = AnthillRuntime::Instance().get<LoginService>();
-					ptr->setCurrentAccessToken(accessToken);
-
-					const online::ExternalAuthenticatorPtr& authenticator = ptr->getExternalAuthenticator();
-
-					if (request.isSuccessful())
-					{
-						online::Credential parsed(credential);
-
-						if (parsed.getCredentialType() != authenticator->getCredentialType())
-						{
-							Log::get() << "Credential types don't match! ";
-							Log::get() << "Parsed credential type=" << parsed.getCredentialType();
-							Log::get() << "External credential type=" << authenticator->getCredentialType() << std::endl;
-							switchTo<AttachExternalAuthenticationState>();
-							return;
-						}
-
-						const ApplicationInfo& applicationInfo = AnthillRuntime::Instance().getApplicationInfo();
-                
-						// check if the requiredScopes are satisfied
-						if (std::includes(scopes.begin(), scopes.end(), applicationInfo.shouldHaveScopes.begin(), applicationInfo.shouldHaveScopes.end()))
-						{
-							Log::get() << "Access token is valid!" << std::endl;
-
-							const ListenerPtr& listener = AnthillRuntime::Instance().getListener();
-
-							if (listener)
-							{
-								listener->authenticated(account, credential, scopes);
-							}
-
-							complete();
-							return;
-						}
-						else
-						{
-							Log::get() << "Required scopes are not satisfied!" << std::endl;
-						}
-					}
-   
-					Log::get() << "Access token is invalid!" << std::endl;
-            
-					AnthillRuntime& online = AnthillRuntime::Instance();
-					StoragePtr storage = online.getStorage();
-
-					storage->remove(Storage::StorageAccessTokeneField);
-					storage->save();
-
-					switchTo<ExternalAuthenticationState>();
-				});
-
-
-			}
-			else
-			{
-				Log::get() << "Access token is not found." << std::endl;
-
-				switchTo<ExternalAuthenticationState>();
-			}
+			switchTo<ExternalAuthenticationState>();
 		}
 	}
 
